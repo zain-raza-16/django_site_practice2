8bd042d1969b7839e815e89bf4e3a7965a4e6d97
Zain Raza <71686254+zain-raza-16@users.noreply.github.com>
2025-01-22 21:38:35 -0600

    clear round

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-path('meetups/',views.index)
-
+  path('meetups', views.index, name='all-meetups'),
+  path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'), 
 ]
